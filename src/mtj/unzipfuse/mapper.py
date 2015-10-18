f12c4ea28fbc4cf9ebbfbf7ac08fa680e2962f65
Tommy Yu <y@metatoaster.com>
2015-10-18 23:52:22 +1300

    A new mapper module
    - This time we have tests to show some basic cases.

---
+++ src/mtj/unzipfuse/mapper.py
@@ -0,0 +1,103 @@
+from logging import getLogger
+from os.path import abspath
+from os.path import basename
+from glob import glob
+from zipfile import ZipFile
+from zipfile import BadZipFile
+
+logger = getLogger(__name__)
+
+
+class NestedMapper(object):
+    """
+    Mapper that tracks the nested structure within a zip file.
+    """
+
+    def __init__(self, path=None):
+        """
+        Initialize the mapping, optionally with a path to a zip file.
+
+        Mapping dict keys are names of file or directory, values are
+        either a tuple that represent a file, or a dict to represent a
+        directory.
+        """
+
+        self.mapping = {}
+        if path:
+            self.load_zip(path)
+
+    def mkdir(self, path_fragments):
+        """
+        Creates the dir entries identified by path if not already exists
+        and return the complete directory.
+        """
+
+        # set current to root node
+        current = self.mapping
+
+        for frag in path_fragments:
+            if not isinstance(current, dict):
+                # make better error message.
+                logger.warning(
+                    'Fail to create directory entry at `%s` blocked by file '
+                    'entry `%s`.',
+                    '/'.join(path_fragments),
+                    frag,
+                )
+                # XXX fix this as this is the WRONG result type.
+                return current
+            if frag in current:
+                # directory dict entry exists, set current to that.
+                current = current[frag]
+                continue
+            # create directory dict entry and set current.
+            current[frag] = current = {}
+
+        return current
+
+    def traverse(self, path):
+        """
+        Traverse to path, or return the entry identified by path.
+        """
+
+        path = '/'.split(path)
+        current = self.mapping
+
+        for frag in path_fragments:
+            if not isinstance(current, dict) or frag not in current:
+                # No such frag in dir.
+                return None
+            current = current[frag]
+
+        return current
+
+    def _load_infolist(self, zipfile_path, infolist):
+        for info in infolist:
+            frags = info.filename.split('/')
+            filename = frags.pop()
+            target = self.mkdir(frags)
+            if not filename:
+                # was a directory entry
+                continue
+            if not isinstance(target, dict):
+                logger.info(
+                    'Could not create `%s` as preceding dir is a file',
+                    info.filename,
+                )
+                continue
+            if filename in target:
+                logger.info('`%s` already exists; ignoring', info.filename)
+                continue
+            target[filename] = (zipfile_path, info.filename, info.file_size)
+
+    def load_zip(self, zipfile_path):
+        """
+        Load a zip file identified by zipfile_path into the mapping.
+        """
+
+        try:
+            with ZipFile(zipfile_path) as zf:
+                self._load_infolist(zipfile_path, zf.infolist())
+        except BadZipFile:
+            logger.warning(
+                '`%s` appears to be an invalid zipfile', zipfile_path)
---
+++ tests/test_mapper.py
@@ -0,0 +1,121 @@
+import unittest
+from os.path import dirname
+from os.path import join
+
+from mtj.unzipfuse.mapper import NestedMapper
+
+
+path = lambda p: join(dirname(__file__), 'data', p)
+
+class NestedMapperTestCase(unittest.TestCase):
+
+    maxDiff = 12300
+
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    def test_mkdir(self):
+        m = NestedMapper()
+        result = m.mkdir(['1', '2', '3'])
+        self.assertEqual(result, m.mapping['1']['2']['3'])
+
+    def test_mapping_simple(self):
+        target = path('demo1.zip')
+        m = NestedMapper(target)
+        self.assertEqual(m.mapping, {
+            'file1': (target, 'file1', 33),
+            'file2': (target, 'file2', 33),
+            'file3': (target, 'file3', 33),
+            'file4': (target, 'file4', 33),
+            'file5': (target, 'file5', 33),
+            'file6': (target, 'file6', 33),
+        })
+
+    def test_mapping_simple_nested(self):
+        target = path('demo2.zip')
+        m = NestedMapper(target)
+        self.assertEqual(m.mapping, {
+            'demo': {
+                'file1': (target, 'demo/file1', 33),
+                'file2': (target, 'demo/file2', 33),
+                'file3': (target, 'demo/file3', 33),
+                'file4': (target, 'demo/file4', 33),
+                'file5': (target, 'demo/file5', 33),
+                'file6': (target, 'demo/file6', 33),
+            }
+        })
+
+    def test_mapping_complex_nested(self):
+        target = path('demo3.zip')
+        m = NestedMapper(target)
+        self.assertEqual(m.mapping['demo'], {
+            'dir1': {
+                'file1': (target, 'demo/dir1/file1', 33),
+                'file2': (target, 'demo/dir1/file2', 33),
+                'file3': (target, 'demo/dir1/file3', 33),
+                'file4': (target, 'demo/dir1/file4', 33),
+            },
+            'dir2': {
+            },
+            'dir3': {
+                'dir3': {
+                    'file5': (target, 'demo/dir3/dir3/file5', 33),
+                },
+            },
+            'dir4': {
+                'dir5': {
+                    'dir6': {
+                        'file6': (target, 'demo/dir4/dir5/dir6/file6', 33),
+                        'dir7': {
+                        },
+                    },
+                },
+            },
+            'some': {
+                'path': (target, 'demo/some/path', 31),
+            },
+            'some_path': (target, 'demo/some_path', 32),
+        })
+
+    def test_mapping_complex_multiple(self):
+        demo3 = path('demo3.zip')
+        demo4 = path('demo4.zip')
+        m = NestedMapper()
+        # load order matters, new entries will not overwrite old ones.
+        m.load_zip(demo3)
+        m.load_zip(demo4)
+        self.assertEqual(list(sorted(m.mapping.keys())), ['demo', 'hello'])
+        self.assertEqual(m.mapping['hello'], (demo4, 'hello', 6))
+        self.assertEqual(m.mapping['demo'], {
+            'dir1': {
+                'file1': (demo3, 'demo/dir1/file1', 33),
+                'file2': (demo3, 'demo/dir1/file2', 33),
+                'file3': (demo3, 'demo/dir1/file3', 33),
+                'file4': (demo3, 'demo/dir1/file4', 33),
+                'file5': (demo4, 'demo/dir1/file5', 26),
+            },
+            'dir2': {
+                'file2': (demo4, 'demo/dir2/file2', 26),
+            },
+            'dir3': {
+                'dir3': {
+                    'file5': (demo3, 'demo/dir3/dir3/file5', 33),
+                },
+            },
+            'dir4': {
+                'dir5': {
+                    'dir6': {
+                        'file6': (demo3, 'demo/dir4/dir5/dir6/file6', 33),
+                        'dir7': {
+                        },
+                    },
+                },
+            },
+            'some': {
+                'path': (demo3, 'demo/some/path', 31),
+            },
+            'some_path': (demo3, 'demo/some_path', 32),
+        })
