fb884fcbf30f06b88d8b7d1e6a764bd93e8201ee
Tommy Yu <y@metatoaster.com>
2015-11-07 02:53:24 +1300

    Symlink creation filesystem
    - A filesystem that support symlinks in one and only one nominated
      directory.
    - Basis for the controller directory.

--- src/explosive/fuse/fs.py
+++ src/explosive/fuse/fs.py
@@ -1,13 +1,18 @@
 import logging
+from os.path import join
 from os.path import abspath
+from os.path import basename
+from os import getcwd
 
 from errno import ENOENT
+from errno import EPERM
 from stat import S_IFDIR
-# from stat import S_IFLNK
+from stat import S_IFLNK
 from stat import S_IFREG
 from time import time
 
 from fuse import FuseOSError, Operations, LoggingMixIn
+from fuse import ENOTSUP
 
 from explosive.fuse.mapper import DefaultMapper
 
@@ -30,6 +35,14 @@ dir_record = dict(
     st_mtime=now,
     st_atime=now,
     st_nlink=2,
+)
+
+link_record = dict(
+    st_mode=(S_IFLNK | 0o444),
+    st_nlink=1,
+    st_ctime=now,
+    st_mtime=now,
+    st_atime=now,
 )
 
 
@@ -92,3 +105,78 @@ class ExplosiveFUSE(LoggingMixIn, Operations):
     def statfs(self, path):
         # TODO report total size of the zips?
         return dict(f_bsize=1024, f_blocks=1024, f_bavail=0)
+
+
+class SymlinkFUSE(LoggingMixIn, Operations):
+    """
+    A symlink only filesystem that exist in memory.
+    """
+
+    def __init__(self, mount_root, base_path='/'):
+        """
+        The base directory where the symlinks are exposed.
+        """
+
+        # assert that base_path starts with '/'.  This is not a key
+        # like the mapping where it's relative to where it's based.
+
+        self.base_path = base_path
+        self.mount_root = mount_root
+        self.fd = 0
+        self.symlinks = {}  # keys are the basename.
+
+    def getattr(self, path, fh=None):
+        if path == '/':
+            path = ''
+        if not path.startswith(self.base_path):
+            # might not be a real path in the system.
+            if (self.base_path.startswith(path) and
+                    self.base_path[len(path)] == '/'):
+                # a valid parent directory to the base_path.
+                return dir_record
+            raise FuseOSError(ENOENT)
+
+        if path == self.base_path:
+            return dir_record
+
+        symkey = basename(path)
+        data = self.symlinks.get(symkey)
+        if data is None:
+            raise FuseOSError(ENOENT)
+        result = {'st_size': len(data)}
+        result.update(link_record)
+        return result
+
+    def create(self, path, mode, fi=None):
+        raise FuseOSError(ENOTSUP)
+
+    def mkdir(self, path, mode):
+        raise FuseOSError(ENOTSUP)
+
+    def readlink(self, path):
+        symkey = basename(path)
+        return self.symlinks[symkey]
+
+    def readdir(self, path, fh):
+        if not path == self.base_path:
+            if path == '/':
+                path = ''
+            return ['.', '..', self.base_path[len(path):].split('/')[1]]
+        return ['.', '..'] + list(self.symlinks.keys())
+
+    def symlink(self, path, source):
+        if not path.startswith(self.base_path):
+            # symlinks must be created inside base_path.
+            raise FuseOSError(EPERM)
+        symkey = basename(path)
+        self.symlinks[symkey] = abspath(join(
+            self.mount_root, self.base_path[1:], source))
+
+    def unlink(self, path):
+        symkey = basename(path)
+        if symkey not in self.symlinks:
+            raise FuseOSError(ENOTSUP)
+        self.symlinks.pop(symkey)
+
+    def statfs(self, path):
+        return {'f_bsize': 0, 'f_blocks': 0, 'f_bavail': 0}
--- tests/test_fs.py
+++ tests/test_fs.py
@@ -5,12 +5,13 @@ from os.path import join
 from fuse import FuseOSError
 
 from explosive.fuse.fs import ExplosiveFUSE
+from explosive.fuse.fs import SymlinkFUSE
 from explosive.fuse import pathmaker
 
 path = lambda p: join(dirname(__file__), 'data', p)
 
 
-class FsTestCase(unittest.TestCase):
+class ExplosiveFsTestCase(unittest.TestCase):
 
     def test_simple(self):
         fs = ExplosiveFUSE([path('demo1.zip')])
@@ -72,4 +73,98 @@ class FsTestCase(unittest.TestCase):
         )
         report = fs.statfs('/')
         # This should always be true.
+        self.assertEqual(report['f_bavail'], 0)
+
+
+class SymlinkFUSETestCase(unittest.TestCase):
+
+    def test_simple(self):
+        fs = SymlinkFUSE('/mnt')
+        self.assertEqual(fs.symlinks, {})
+        self.assertEqual(fs.base_path, '/')
+        self.assertEqual(fs.mount_root, '/mnt')
+
+    def test_getattr(self):
+        fs = SymlinkFUSE('/mnt')
+        self.assertEqual(fs.getattr('/')['st_mode'], 0o40555)
+        with self.assertRaises(FuseOSError):
+            fs.getattr('/no_such_file')
+
+        fs = SymlinkFUSE('/mnt', base_path='/.control')
+        self.assertEqual(fs.getattr('/')['st_mode'], 0o40555)
+        self.assertEqual(fs.getattr('/.control')['st_mode'], 0o40555)
+        with self.assertRaises(FuseOSError):
+            fs.getattr('/.control/here')
+        with self.assertRaises(FuseOSError):
+            fs.getattr('/.cont')
+
+        fs.symlinks['here'] = '/home/somewhere'
+        self.assertEqual(fs.getattr('/.control/here')['st_mode'], 0o120444)
+
+    def test_create(self):
+        fs = SymlinkFUSE('/mnt', base_path='/.control')
+        with self.assertRaises(FuseOSError):
+            fs.create('/.cont', 0)
+
+    def test_mkdir(self):
+        fs = SymlinkFUSE('/mnt', base_path='/.control')
+        with self.assertRaises(FuseOSError):
+            fs.mkdir('/.cont', 0)
+
+    def test_readlink(self):
+        fs = SymlinkFUSE('/mnt', base_path='/.control')
+        fs.symlinks['here'] = '/home/somewhere'
+        self.assertEqual(fs.readlink('/.control/here'), '/home/somewhere')
+
+    def test_readdir(self):
+        fs = SymlinkFUSE('/mnt')
+        self.assertEqual(fs.readdir('/', 0), ['.', '..'])
+
+        fs = SymlinkFUSE('/mnt', base_path='/some/nested/structure')
+        self.assertEqual(fs.readdir('/', 0), ['.', '..', 'some'])
+        self.assertEqual(fs.readdir('/some', 0), ['.', '..', 'nested'])
+        self.assertEqual(
+            fs.readdir('/some/nested', 0), ['.', '..', 'structure'])
+        self.assertEqual(
+            fs.readdir('/some/nested/structure', 0), ['.', '..'])
+
+        fs.symlinks['here'] = '/home/somewhere'
+        self.assertEqual(
+            fs.readdir('/some/nested/structure', 0), ['.', '..', 'here'])
+
+    def test_symlink(self):
+        fs = SymlinkFUSE('/mnt')
+        fs.symlink('/somewhere/else', '/target/to/file')
+        self.assertEqual(fs.symlinks['else'], '/target/to/file')
+        # relative links gets resolved to absolute
+        fs.symlink('/somewhere/else', 'target')
+        self.assertEqual(fs.symlinks['else'], '/mnt/target')
+
+        fs = SymlinkFUSE('/mnt/somewhere', base_path='/some/nested/structure')
+
+        with self.assertRaises(FuseOSError):
+            fs.symlink('/somewhere/else', '/target')
+
+        with self.assertRaises(FuseOSError):
+            fs.symlink('/some/nested/struct', '/target')
+
+        fs.symlink('/some/nested/structure/target', '/target')
+        self.assertEqual(fs.symlinks['target'], '/target')
+
+        fs.symlink('/some/nested/structure/target2', '../../../../target')
+        self.assertEqual(fs.symlinks['target2'], '/mnt/target')
+
+    def test_unlink(self):
+        fs = SymlinkFUSE('/mnt')
+        fs.symlinks['else'] = '/home/else'
+        fs.unlink('/else')
+        self.assertEqual(fs.symlinks, {})
+
+        fs = SymlinkFUSE('/mnt', base_path='/a/dir')
+        with self.assertRaises(FuseOSError):
+            fs.unlink('/a/dir')
+
+    def test_statfs(self):
+        fs = SymlinkFUSE('/mnt')
+        report = fs.statfs('/')
         self.assertEqual(report['f_bavail'], 0)
